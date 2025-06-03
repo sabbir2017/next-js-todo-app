b4dd0761819addea87df053c4f67c7b168c84c1a
sabbir2017 <sabbirhasan74@gmail.com>
2025-06-04 01:14:27 +0600

    Add Update Delete action added

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 "use client";
 
-import { useState } from "react";
+import { use, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
 import "@/app/globals.css";
 import "@/app/page.css";
 
@@ -35,6 +36,9 @@ const todos = [
 
 export default function Home() {
   const [data, setData] = useState(todos);
+  const [title, setTitle] = useState("");
+  const [msg, setMsg] = useState("");
+  const [selected, setSelected] = useState({});
 
   const handleCheck = (selectedItem) => {
     const newItem = data.map((item) => {
@@ -44,21 +48,94 @@ export default function Home() {
     });
     setData(newItem);
   };
+
+  const handleAddTodo = () => {
+    if (title === "") {
+      setMsg("Please add todo");
+    } else {
+      const newTodo = {
+        id: data.length + 1,
+        title: title,
+        isComplete: false,
+      };
+      setData([...data, newTodo]);
+      toast("Todo added successfully");
+      setTitle("");
+      setMsg("");
+    }
+  };
+
+  const handleAddInput = (e) => {
+    setTitle(e.target.value);
+    setMsg("");
+  };
+
+  const handleEdit = (editItem) => {
+    setTitle(editItem.title);
+    setSelected(editItem);
+  };
+
+  const handleUpdate = () => {
+    const newItem = data.map((item) => {
+      return item.id === selected.id ? { ...item, title: title } : item;
+    });
+    setData(newItem);
+    toast("Todo updated successfully");
+    setTitle("");
+    setSelected({});
+  };
+
+  const handleDelete = (deleteItem) => {
+    const newDeleteItem = data.filter(item=>{
+      return item.id != deleteItem.id
+    })
+    setData(newDeleteItem);
+  };
+
   return (
     <div className="page">
-      <div className="max-w-xl mx-auto p-10 bg-white mt-10 rounded-2xl">
+      <div className="max-w-4xl mx-auto p-10 bg-white mt-10 rounded-2xl">
         <h1 className="text-center text-4xl font-bold mb-8">Todo List</h1>
+        <div className="mb-5">
+          <div className="flex mb-1">
+            <ToastContainer />
+            <input
+              type="text"
+              placeholder="Enter your todo"
+              value={title}
+              onChange={handleAddInput}
+              className=" flex-auto border p-2 rounded-tl-md rounded-bl-md"
+            />
+            {!selected.id ? (
+              <button
+                onClick={handleAddTodo}
+                className=" bg-green-600 p-2 text-white rounded-tr-md rounded-br-md cursor-pointer"
+              >
+                Add Todo
+              </button>
+            ) : (
+              <button
+                onClick={handleUpdate}
+                className=" bg-green-600 p-2 text-white rounded-tr-md rounded-br-md cursor-pointer"
+              >
+                Update Todo
+              </button>
+            )}
+          </div>
+          {msg && <p className=" text-red-600">{msg}</p>}
+        </div>
         <ul className="list-none">
           {data.map((item) => (
             <li
-              className="bg-purple-200 mb-2 rounded-sm p-2 flex justify-between items-center"
+              className="bg-purple-200 mb-2 rounded-sm p-2 flex gap-2 justify-between items-center"
               key={item.id}
             >
-              <div className="flex gap-2 items-center">
+              <div className="flex gap-2 items-center flex-auto">
                 <input
                   type="checkbox"
                   onChange={() => handleCheck(item)}
                   checked={item.isComplete}
+                  className="cursor-pointer"
                 />
                 <p className={item.isComplete ? "line-through" : ""}>
                   {item.title}
@@ -70,6 +147,20 @@ export default function Home() {
                 } whitespace-nowrap p-2 rounded-sm text-white`}
               >
                 Status: {item.isComplete ? "Completed" : "Pending"}
+              </div>
+              <div className="flex gap-2">
+                <button
+                  onClick={() => handleEdit(item)}
+                  className=" bg-cyan-500 p-2 text-white rounded-md cursor-pointer"
+                >
+                  Edit
+                </button>
+                <button
+                  onClick={() => handleDelete(item)}
+                  className=" bg-red-600 p-2 text-white rounded-md cursor-pointer"
+                >
+                  Delete
+                </button>
               </div>
             </li>
           ))}
