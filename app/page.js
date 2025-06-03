8507477ab59895678555d18a99bac6141f523a6b
sabbir2017 <sabbirhasan74@gmail.com>
2025-06-03 23:20:02 +0600

    todos check mark when complete a todo

--- app/page.js
+++ app/page.js
@@ -1,103 +1,80 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
+import "@/app/globals.css";
+import "@/app/page.css";
+
+const todos = [
+  {
+    id: 1,
+    title:
+      "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+    isComplete: false,
+  },
+  {
+    id: 2,
+    title: "quis ut nam facilis et officia qui",
+    isComplete: true,
+  },
+  {
+    id: 3,
+    title: "fugiat veniam minus",
+    isComplete: false,
+  },
+  {
+    id: 4,
+    title: "et porro tempora",
+    isComplete: true,
+  },
+  {
+    id: 5,
+    title: "laboriosam mollitia et enim quasi adipisci quia provident illum",
+    isComplete: false,
+  },
+];
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [data, setData] = useState(todos);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  const handleCheck = (selectedItem) => {
+    const newItem = data.map((item) => {
+      return item.id === selectedItem.id
+        ? { ...item, isComplete: !item.isComplete }
+        : item;
+    });
+    setData(newItem);
+  };
+  return (
+    <div className="page">
+      <div className="max-w-xl mx-auto p-10 bg-white mt-10 rounded-2xl">
+        <h1 className="text-center text-4xl font-bold mb-8">Todo List</h1>
+        <ul className="list-none">
+          {data.map((item) => (
+            <li
+              className="bg-purple-200 mb-2 rounded-sm p-2 flex justify-between items-center"
+              key={item.id}
+            >
+              <div className="flex gap-2 items-center">
+                <input
+                  type="checkbox"
+                  onChange={() => handleCheck(item)}
+                  checked={item.isComplete}
+                />
+                <p className={item.isComplete ? "line-through" : ""}>
+                  {item.title}
+                </p>
+              </div>
+              <div
+                className={`${
+                  item.isComplete ? "bg-green-800" : "bg-purple-500"
+                } whitespace-nowrap p-2 rounded-sm text-white`}
+              >
+                Status: {item.isComplete ? "Completed" : "Pending"}
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
